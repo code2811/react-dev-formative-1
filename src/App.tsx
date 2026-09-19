6c69a9e35f33d050198de0c29028f9168a820ef0
Your Name <f.kayiranga@alustudent.com>
2026-09-19 20:07:55 +0200

    Wire up App component and global styles

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+import PostList from './components/PostList';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <PostList />
+    </div>
+  );
+}
+
+export default App;
